733c17dc050f8bf3523d626253ecb4fbcb1fee26
João Gabriel Berti <127428856+Jgabrielberti@users.noreply.github.com>
2023-04-13 13:44:07 -0300

    aula 13-04-23 - programa testes

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("Hello World!")
+
+const entrada = require('prompt-sync')();
+
+console.log("Hello World!");
+
+let nome = entrada('Digite seu nome: ');
+console.log('Olá ' + nome);
+
+let idadeTxt = entrada(nome + ', quantos anos você tem? ');
+let idade = parseInt(idadeTxt)
+
+let resposta = 'n';
+if (idade < 15){
+    resposta = entrada('Acredito que você esteja no ensino fundamental. \nEstou certo? (responda s/n)');
+}else if (idade > 15 && idade < 18){
+    resposta = entrada('Acredito que você esteja no ensino médio. \nEstou certo? (responda s/n)');
+}else{
+    resposta = entrada('Acredito que você já tenha terminado o ensino médio. \nEstou certo? (responda s/n)');
+}
+
+if (resposta == 'n') {
+    console.log('Ok, eu não sei de tudo');
+}else{
+    console.log('Legal, quando eu crescer vou ser igual o CHAT GPT!')
+}
+
+entrada("Fim do programa, pressione ENTER para encerrar!")
