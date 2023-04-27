ca8ce3f6d12f67b3fa8d6827210408038d411694
João Gabriel Berti <127428856+Jgabrielberti@users.noreply.github.com>
2023-04-27 13:50:46 -0300

    aula 27/04

--- index.js
+++ index.js
@@ -14,7 +14,9 @@ if (idade < 15) {
     resposta = entrada('Acredito que você esteja no ensino fundamental. \nEstou certo? (responda s/n)');
 } else if (idade > 15 && idade < 18) {
     resposta = entrada('Acredito que você esteja no ensino médio. \nEstou certo? (responda s/n)');
-} else {
+}else if (idade >= 110) {
+    resposta = entrada('Acredito que você seja um fóssil, estou certo? (responda s/n)');
+}else {
     resposta = entrada('Acredito que você já tenha terminado o ensino médio. \nEstou certo? (responda s/n)');
 }
 
@@ -26,3 +28,4 @@ if (resposta === 'n') {
 
 console.log('');
 entrada('Fim do programa, pressione ENTER para encerrar!');
+
